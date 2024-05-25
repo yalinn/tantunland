fa8c3c6ba37b23c5afbdd37719b861e8dc910d65
Orhan Yalin ozturk <orhanyalin@hotmail.com>
2024-05-26 01:59:20 +0300

    a

--- src/apps/registry/events/puan.js
+++ src/apps/registry/events/puan.js
@@ -20,6 +20,7 @@ class PrefixCommandCreate extends ClientEvent {
     async run(message) {
         const channelData = this.client.models.channels.findOne({ meta: { $elemMatch: { id: message.channel.id } } });
         const points = channelData.points;
+        if (!channelData || !points) return;
         await this.client.models.user_points.findOneAndUpdate({ userId: message.author.id }, { $inc: { points: points } }, { upsert: true });
     }
 }
