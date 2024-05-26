3cba33b4bcfe4f8db21fa70c9ebabc9a65da8c66
Orhan Yalin ozturk <orhanyalin@hotmail.com>
2024-05-27 00:04:05 +0300

    models cleared

--- src/base/models.js
+++ src/base/models.js
@@ -1,59 +1,58 @@
-const { escapeNumberedList } = require("discord.js");
+const config = require("./config.js");
 const { model, Schema } = require("mongoose");
 
 
 
 module.exports = {
-    sunucu: model("server_data", new Schema({
-        guildId: String,
-        tag: String,
-        untag: String,
-        otoregister: Boolean,
-        taglı: Boolean,
-        register: Boolean
-    })),
+
     member: model("meta_members", new Schema({
         _id: String,
+        guildId: { type: String, default: config.guildId },
         roles: [String],
-        names: [{       // Tag aldırma olayı
+        names: [{
             username: String,
             displayName: String,
             created: Date,
             claimer: String
         }],
-        registries: [{  //  kayıt sayacı
-            created: Date,
-            executor: String,
-            gender: String,
-            age: String
-        }],
         deleted: Boolean
-    }, { _id: false })),
+    }, {
+        _id: false,
+        timestamps: {
+            createdAt: "created_at",
+            updatedAt: "updated_at"
+        },
+        versionKey: false
+    })),
+
     banned_tag: model("yasaklitag", new Schema({
         guildId: String,
         tags: Array,
         discriminators: Array
+    }, {
+        timestamps: {
+            createdAt: "created_at",
+            updatedAt: "updated_at"
+        },
+        versionKey: false
     })),
-    snipe: model("snipe", new Schema({
-        guildId: String,
-        channelId: String,
-        userId: String,
-        message: String,
-        send: Number,
-        deleted: Number,
-    })),
+
     afk: model("afk_data", new Schema({
-        guildId: String,
+        guildId: { type: String, default: config.guildId },
         memberId: String,
         date: Date,
         reason: String,
         status: Boolean
+    }, {
+        timestamps: {
+            createdAt: "created_at",
+            updatedAt: "updated_at"
+        },
+        versionKey: false
     })),
-    commandblock: model("block_commands", new Schema({
-        guildId: String,
-        cb: Array
-    })),
+
     penalties: model("data_penalty", new Schema({
+        guildId: { type: String, default: config.guildId },
         userId: String,
         executor: String,
         reason: String,
@@ -63,56 +62,38 @@ module.exports = {
         expired: Boolean
     }, {
         timestamps: {
-            createdAt: "created"
-        }
+            createdAt: "created_at",
+            updatedAt: "updated_at"
+        },
+        versionKey: false
     })),
+
     invites: model("stat_invite", new Schema({
-        guildId: String,
+        guildId: { type: String, default: config.guildId },
         inviterId: String,
         invitedId: String,
         urlCode: String,
         left: Boolean
     }, {
         timestamps: {
-            created: "created",
-            updatedAt: "leftDate"
-        }
+            created: "created_at",
+            updatedAt: "updated_at"
+        },
+        versionKey: false
     })),
-    register: model("register_data", new Schema({
-        guildId: String,
+
+    registry: model("registry_data", new Schema({
+        guildId: { type: String, default: config.guildId },
         userId: String,
-        user: Array,
-        rol: Array,
-        name: Array,
-        top: Number,
-        default: String
-    })),
-    punishment: model("Punishment", new Schema({
-        guildId: String,
-        userId: String,
-        topunish: Number,
-        ban: Number,
-        punishpoint: Number,
-        mute: Number,
-        vmute: Number,
-        jail: Number,
-        underwold: Number,
-        mutestatus: Boolean,
-        allstatus: Boolean,
-        vmutestatus: Boolean,
-        jailstatus: Boolean,
-        underwoldstatus: Boolean
-    })),
-    penals: model("Penals", new Schema({
-        penalid: Number,
-        guildId: String,
-        userId: String,
-        type: String,
-        status: Boolean,
-        staff: String,
-        reason: String,
-        end: Date,
-        date: Date
+        staffId: String,
+        fromRoleId: [String],
+        disabled: { default: false, type: Boolean }
+    }, {
+        timestamps: {
+            created: "created_at",
+            updatedAt: "updated_at"
+        },
+        versionKey: false
     })),
 
     roles: model("meta_roles", new Schema({
@@ -133,6 +114,7 @@ module.exports = {
         deleted: Boolean,
         emojis: [String]
     })),
+
     channels: model("meta_channels", new Schema({
         keyConf: String,
         type: Number,
@@ -150,11 +132,13 @@ module.exports = {
     }, {
         strict: false
     })),
+
     user_points: model("user_points", new Schema({
         guildId: String,
         userId: String,
         points: Number
     })),
+
     chat_logs: model("chat_logs", new Schema({
         guildId: String,
         channelId: String,
