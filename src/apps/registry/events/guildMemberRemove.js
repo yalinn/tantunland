bb1266e5408f1fb11ecd35dc862ed52868fad626
Orhan Yalin ozturk <orhanyalin@hotmail.com>
2024-06-12 00:46:35 +0300

    a

--- src/apps/registry/events/guildMemberRemove.js
+++ src/apps/registry/events/guildMemberRemove.js
@@ -19,17 +19,23 @@ class GuildMemberRemove extends ClientEvent {
         const memberData = await this.client.models.member.findOne({ _id: member.id });
         await this.client.models.member.updateOne({ _id: member.id }, { $set: { roles: [] } });
         const inviteData = await this.client.models.invites.findOne({ memberId: member.id, guildId: member.guild.id, left: false });
-        if (!inviteData) return;
+        if (!inviteData) {
+            console.log(`${member.user.tag} sunucudan ayrıldı. Davetçisi bulunamadı.`);
+            return;
+        }
         await this.client.models.invites.updateMany({ invitedId: member.id, guildId: member.guild.id, left: false }, { $set: { left: true } });
-        if (!memberData) return;
+        if (!memberData) {
+            console.log(`${member.user.tag} sunucudan ayrıldı. Davetçisi bulunamadı.`);
+            return;
+        }
         const colors = {
             member: "#ff0000",
             guest: "#FF8C69",
             stranger: "#ff5349",
             autorole: "#800000",
         }
-        const the_role = Object.keys(colors).find(r => member.roles.cache.has(this.client.data.roles[r]));
-        if (!the_role) return;
+        const the_role = Object.keys(colors).find(r => member.roles.cache.has(this.data.roles[r]));
+        if (!the_role) the_role = "autorole";
         const welcome_channel = member.guild.channels.cache.get(inviteData.channel_id);
         if (!welcome_channel) return;
         const message = await welcome_channel.messages.fetch(inviteData.message_id);
